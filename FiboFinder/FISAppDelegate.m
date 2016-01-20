3e635cc396b45b0aa2f4f15f9cd9530ba2b755af
joshmotley <joshkmotley@gmail.com>
2016-01-20 02:12:54 -0500

    create method that calculates the Fibonacci Sequence

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -15,4 +15,25 @@
  
  */
 
+-(NSArray *)arrayWithFibonacciSequenceToIndex:(NSUInteger)index{
+    
+    NSMutableArray *sequence = [[NSMutableArray alloc]init];;
+    for (NSUInteger i = 0; i<index +1; i++) {
+        if(i==0){
+            [sequence addObject:@0];
+        }else if (i==1) {
+            [sequence addObject:@1];
+        }else{
+            NSNumber *s = [sequence objectAtIndex:i-2];
+            NSNumber *s1 = [sequence objectAtIndex:i-1];
+            NSUInteger sum = [s1 integerValue] + [s integerValue];
+            NSNumber *numberSum = @(sum);
+            [sequence addObject:numberSum];
+        }
+    }
+    
+    
+    return sequence;
+}
+
 @end
